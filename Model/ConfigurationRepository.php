53d3fecbe4dd76a70f0db8e7fb1f0c6889a7223f
travish <traviswaynehill@gmail.com>
2019-03-21 12:56:03 -0500

    Add a configuration API endpoint /V1/zaius/engage/configuration.

---
+++ Model/ConfigurationRepository.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Trellis
+ * Date: 3/21/2019
+ * Time: 12:12 PM
+ */
+
+namespace Zaius\Engage\Model;
+
+
+class ConfigurationRepository
+{
+    public function getList()
+    {
+        return 'Zaius Engage ConfigurationRepository';
+    }
+}
