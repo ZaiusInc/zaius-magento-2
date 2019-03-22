7db0fe135a835876ca298c56933518e2d8518930
travish <dev@traviswhill.com>
2019-03-22 11:26:46 -0500

    Add logic to the SystemConfigObserver for testing purposes.

--- Observer/SystemConfigObserver.php
+++ Observer/SystemConfigObserver.php
@@ -7,6 +7,8 @@ use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Zaius\Engage\Helper\Data;
 use Zaius\Engage\Logger\Logger;
+//todo remove later
+use Zaius\Engage\Model\SchemaRepository;
 
 class SystemConfigObserver
     implements ObserverInterface
@@ -15,21 +17,32 @@ class SystemConfigObserver
     protected $_helper;
     protected $_logger;
 
+    //todo remove later
+    protected $_schemaRepository;
+
     public function __construct(
         StoreManagerInterface $storeManager,
         Data $helper,
-        Logger $logger
+        Logger $logger,
+
+        //todo remove later
+        SchemaRepository $schemaRepository
     )
     {
         $this->_storeManager = $storeManager;
         $this->_helper = $helper;
         $this->_logger = $logger;
+
+        //todo remove later
+        $this->_schemaRepository = $schemaRepository;
     }
 
     public function execute(Observer $observer)
     {
         if ($this->_helper->getStatus($this->_storeManager->getStore())) {
             $this->_logger->info("WE'RE LOGGING FROM THE NEW OBSERVER!!");
+            //todo change _schemaRepository in the future
+            $this->_schemaRepository->upsertObjects();
         }
     }
 }
