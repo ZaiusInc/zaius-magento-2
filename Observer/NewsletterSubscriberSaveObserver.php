e137d5782a325efbb82af6c0116756b9b3f1bea6
Rafael Corrêa Gomes <rafaelcgstz@gmail.com>
2019-08-06 15:09:31 -0300

    [ZAIR-153] Subscribe > Log > Change info to warning

--- Observer/NewsletterSubscriberSaveObserver.php
+++ Observer/NewsletterSubscriberSaveObserver.php
@@ -102,7 +102,7 @@ class NewsletterSubscriberSaveObserver implements ObserverInterface
             if (($ts = strtotime($subscriber->getChangeStatusAt())) !== false) {
                 $event['data']['ts'] = $ts;
             } else {
-                $this->_logger->info('Wrong timestamp reported by  Zaius\Engage\Observer\NewsletterSubscriberSaveObserver class, the getChangeStatusAt() method returned: '.print_r($subscriber->getChangeStatusAt()));
+                $this->_logger->warning('Wrong timestamp reported by  Zaius\Engage\Observer\NewsletterSubscriberSaveObserver class, the getChangeStatusAt() method returned: '.print_r($subscriber->getChangeStatusAt()));
             }
 
             $state = $this->_state->getAreaCode();
