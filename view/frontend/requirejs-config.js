c3fe57ed9a5bde14bb93ce69dd430b5fd162d656
Rafael Corrêa Gomes <rafaelcgstz@gmail.com>
2019-09-04 21:57:58 -0300

    [ZAIR-157] JS > New zaius file + Mapping the zaius-min via CDN

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    map: {
+        '*': {
+            'zaius': 'Zaius_Engage/js/zaius'
+        }
+    },
+    paths: {
+        'zaius-min': '//d1igp3oop3iho5.cloudfront.net/v2/piUzfdlpdHM-rtbS6OpeCQ/zaius-min'
+    }
+};
