bfd762c2ee413efe54b9c3943906a4ce3a7f59d6
Tom Mollerus <tmollerus@zaius.com>
2020-09-01 19:44:56 -0400

    Changed default vtsrc variable to empty array instead of null

--- Helper/Data.php
+++ Helper/Data.php
@@ -395,7 +395,7 @@ class Data extends AbstractHelper
     public function getVTSRC()
     {
         $vtsrcCookie = $this->_cookieManager->getCookie('vtsrc');
-        $vtsrc = null;
+        $vtsrc = [];
         if ($vtsrcCookie) {
             $vtsrc = $this->prepareVtsrc($vtsrcCookie);
         }
